3ce0a923d1f65e2eed385f310a9bc5955cd28d5f
Bruno-Serafim <br.serafim.27@gmail.com>
2020-08-02 19:05:19 -0300

    Iniciando aplicação

--- 3.app-de-todos/todos.js
+++ 3.app-de-todos/todos.js
@@ -1,1 +1,9 @@
-alert('Funcionou')
+var listElement = document.querySelector('#App ul')
+var inputElement = document.querySelector('#App input')
+var buttonElement = document.querySelector('#App button')
+
+var todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocketseat'
+]
