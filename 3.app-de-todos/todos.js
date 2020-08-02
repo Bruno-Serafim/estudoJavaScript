26c9cebb657c78ca6db5dd842e2fdeb97d9bdf5c
Bruno-Serafim <br.serafim.27@gmail.com>
2020-08-02 20:06:15 -0300

    Excluindo Todos

--- 3.app-de-todos/todos.js
+++ 3.app-de-todos/todos.js
@@ -13,7 +13,20 @@ function renderTodos(){
     for(todo of todos){
         var todoElement = document.createElement('li')
         var todoText = document.createTextNode(todo)
+
+        var linkElement = document.createElement('a')
+
+        linkElement.setAttribute('href', '#')
+
+        var pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', `deleteTodo(${pos})`)
+
+        var linkText = document.createTextNode('Excluir')
+
+        linkElement.appendChild(linkText)
+
         todoElement.appendChild(todoText)
+        todoElement.appendChild(linkElement)
         listElement.appendChild(todoElement)
     }
 }
@@ -29,3 +42,8 @@ function addTodos(){
 }
 
 buttonElement.onclick = addTodos
+
+function deleteTodo(pos){
+    todos.splice(pos, 1)
+    renderTodos()
+}
